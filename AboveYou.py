a30b453c356faaaa111d3b0c9a31c046a0444ba6
Cyberwizard-v <veke2003@Outlook.com>
2022-09-28 21:16:04 +0200

    + init

--- AboveYou.py
+++ AboveYou.py
@@ -2,11 +2,12 @@ import functions
 import time
 
 def main():
-    mainscreen = True
-    gamestart = False
+    mainscreen = False
+    gamestart = True
     gameInventory = []
+
     while mainscreen:
-        functions.playSound("music/minecraftmusic.mp3", 1)
+        #functions.playSound("music/minecraftmusic.mp3", 1)
         functions.clearConsole()
         functions.intro()
         time.sleep(5)
@@ -14,19 +15,19 @@ def main():
     gamestart = True
 
     while gamestart:
-        y = functions.chooseSys("Egel", "bom", "bom", "checkitems")
+        y = functions.chooseSys("Egel", "bom", "bom", "checkitems", "quit")
         if y == "Egel":
             userChoose = "Egel"
             gameInventory.append(userChoose)
             functions.typeSys(f"""\nYou have obtained a Item {gameInventory[gameInventory.index(userChoose)]}""", 0.01)
         elif y == "checkitems":
             print(gameInventory)
+        elif y == "quit":
+            print("quit game")
+            break
         else:
             print("Bruh stupid?")
         
 main()
 
-#test
-#Je bent super dik
 
-#test
