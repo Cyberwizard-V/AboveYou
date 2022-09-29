fee2a70e84d206ed6d9ed809528b9acb159a7cff
Jeffrey Yeh <yehspam@outlook.com>
2022-09-29 16:10:01 +0200

    29/09/2022 AboveYou, Hell almost finished

--- AboveYou.py
+++ AboveYou.py
@@ -1,17 +1,17 @@
 import functions
 import time
+from functions import *
 
 def main():
-    mainscreen = False
-    stageHell = True
-    gameInventory = []
+    mainscreen = True
+    stageHell = False
     hitpoints = 3
 
     while mainscreen:
         functions.playSound("music/minecraftmusic.mp3", 1)
         functions.clearConsole()
         functions.intro()
-        time.sleep(5)
+        time.sleep(3)
         break
     stageHell = True
 
@@ -23,82 +23,101 @@ It could be a thousand degrees in here.
 Suddenly the stench of burning body’s and sulphur is rushing into your nose. 
 I need to find a way out of here. 
 You decide to wander around and look for hints to escape this place.""", 0.001)
-        time.sleep(1)
+        time.sleep(5)
 
         functions.clearConsole()
 
         functions.typeSys(f"""It is almost unbearable to walk around properly, due to the temperature.
 I should look for something, which can help me against the heat.""", 0.001)
-        time.sleep(0)
+        time.sleep(5)
         functions.clearConsole()
 
         functions.typeSys(f"""You look around, and see\n""", 0.001)
-        
         options = [0, 0 ,0, 0, 0]
-
-        while True:
-            y = functions.chooseSys("suspicious chest", "a corpse", "lost soul", "mix tapes", "a wall", "Main")
+        optionsC = [0, 0 ,0, 0, 0]
+        environmentHell = True
+        while environmentHell:
+            y = functions.chooseSys("suspicious chest", "a corpse", "mix tapes", "large door")
             if y == "suspicious chest":
                 options[0] += 1
                 if options[0] > 1:
-                    print("Bruh you already chose this dumbass")
+                    functions.typeSys("Bruh you already chose this dumbass")
                 else:
-                    
                     functions.typeSys(f"""You attempt to inspect the suspicious chest, the chest starts to smile and show its teeth when you get closer.
 Its too late to jump out of the way, you get bitten by the chest. Just like Darksouls.""", 0.001)
-                    print("You lost 1 hp")
+                    functions.typeSys("You lost 1 hp")
                     hitpoints -= 1
-                    print(f"Your current hp is {hitpoints} hp")
+                    functions.typeSys(f"Your current hp is {hitpoints} hp")
+            elif y == "Back":
+                functions.typeSys("You can't go back here.")
+            elif y == "mix tapes":
+                functions.typeSys("We open this and play a mix tape earrape?, after this you lose 1 hp?")
+            elif y == "large door":
+                if functions.playerStatus[0] == 0:
+                    functions.typeSys("Its too hot to touch")
+                else:
+                    while True:
+                        functions.typeSys(f"""You get blinded by the light. Your eyes and body get used to the ambience of the room.
+You open the large door and reveal a huge throne room in front of you, it looks like someone has been waiting for you""", 0.001)
+                    
+                        
             elif y == "a corpse":
                 functions.typeSys(f"""The corpse is hanging from the ceiling, it looks like an old man. 
 It reminds you of your dad, which went to the market to get milk 10 years ago... 
 You look if there is anything to salvage, from his belongings: \n""", 0.001)
-
-                optionsC = [0,0,0,0]
-                time.sleep(1)
-                while True:
-                    functions.clearConsole()
-                    print("Choose a item to inspect or pickup:")
+                corpse = True
+                while corpse:
+                    # functions.clearConsole()
+                    functions.typeSys("Choose a item to inspect or pickup:")
                     print("#"* 100)
-                    y1 = functions.chooseSys("jar of milk", "adoption papers", "car keys", "picture of your mom", "Intestines")
+                    y1 = functions.chooseSys("jar of milk", "adoption papers", "car keys", "picture of your mom", "Menu", "Back")
                     if y1 == "jar of milk":
                         optionsC[0] += 1
                         if optionsC[0] > 1:
-                            print("You already picked this item up.")
+                            functions.typeSys("You already picked this item up jar of milk.")
                         else: 
-                            gameInventory.append("jar of milk")
-                            functions.typeSys(f"""You have obtained a Item {gameInventory[gameInventory.index(y1)]}\n""", 0.01)
-                    if y1 == "addoption papers":
+                            functions.gameInventory.append("jar of milk")
+                            functions.typeSys(f"""You have obtained a Item {functions.gameInventory[functions.gameInventory.index(y1)]}\n""", 0.01)
+                    elif y1 == "adoption papers":
                         optionsC[1] += 1
                         if optionsC[1] > 1:
-                            print("You already picked this item up.")
-                        gameInventory.append("adoption papers")
-                        functions.typeSys(f"""You have obtained a Item {gameInventory[gameInventory.index(y1)]}\n""", 0.01)
-                    if y1 == "car keys":
+                            functions.typeSys("You already picked this item up.")
+                        else:
+                            functions.gameInventory.append("adoption papers")
+                            functions.typeSys(f"""You have obtained a Item {functions.gameInventory[functions.gameInventory.index(y1)]}\n""", 0.01)
+                    elif y1 == "car keys":
                         optionsC[2] += 1
                         if optionsC[2] > 1:
-                            print("You already picked this item up.")
-                        gameInventory.append("car keys")
-                        functions.typeSys(f"""You have obtained a Item {gameInventory[gameInventory.index(y1)]}\n""", 0.01)
-                    if y1 == "picture of your mom":
+                            functions.typeSys("You already picked this item up.")
+                        else:
+                            functions.gameInventory.append("car keys")
+                            functions.typeSys(f"""You have obtained a Item {functions.gameInventory[functions.gameInventory.index(y1)]}\n""", 0.01)
+                    elif y1 == "picture of your mom":
                         optionsC[3] += 1
                         if optionsC[3] > 1:
-                            print("You already picked this item up.")
-                        gameInventory.append("picture of your mom")
-                        functions.typeSys(f"""You have obtained a Item {gameInventory[gameInventory.index(y1)]}\n""", 0.01)
-                    if y1 == "Intestines":
-                        optionsC[4] += 1
-                        if optionsC[4] > 1:
-                            print("AGAIN, nothing to find here.")
-                        else: 
-                            print("Nothing to find here...")
-            elif y == "map":
-                functions.gameMap(0)
-            elif y == "quit":
-                print("quit game") 
-                break
-            else:
-                print("Bruh stupid?")
+                            functions.typeSys("You already picked this item up.")
+                        else:
+                            functions.gameInventory.append("picture of your mom")
+                            functions.typeSys(f"""You have obtained a Item {functions.gameInventory[functions.gameInventory.index(y1)]}\n""", 0.01)
+                    elif y1 == "Menu":
+                            functions.menu("Inventory", "Map")
+                    elif y1 == "Back":
+                        break
+                    else:
+                        functions.typeSys("Not a valid option")
+                        
+            elif y == "Menu":
+                    functions.menu("Inventory", "Map")
+                
+                            
+                            # if y2 == "Inventory":
+                            #     functions.typeSys(functions.gameInventory)
+                            # elif y2 == "Map":
+                            #     functions.typeSys(f"You haven't found a map yet")
+                            # else:
+                            #     functions.typeSys("Break")
+
+
         
 
         # y = functions.chooseSys("Egel", "bom", "bom", "checkInventory", "map", "quit")
@@ -108,14 +127,14 @@ You look if there is anything to salvage, from his belongings: \n""", 0.001)
         #     functions.typeSys(
         #         f"""\nYou have obtained a Item {gameInventory[gameInventory.index(userChoose)]}""", 0.01)
         # elif y == "checkInventory":
-        #     print(gameInventory)
+        #     functions.typeSys(gameInventory)
         # elif y == "map":
         #     functions.gameMap(0)
         # elif y == "quit":
-        #     print("quit game")
+        #     functions.typeSys("quit game")
         #     break
         # else:
-        #     print("Bruh stupid?")
+        #     functions.typeSys("Bruh stupid?")
 
 
 main()
--- functions.py
+++ functions.py
@@ -5,6 +5,11 @@ from time import sleep
 from pygame import mixer
 
 
+gameInventory = []
+playerStats = [0]
+
+
+
 def intro():
     typeSys(f"""   
       __    ____  _____  _  _  ____    _  _  _____  __  __ 
@@ -21,7 +26,7 @@ def intro():
         Your view is getting hazy and slowly fading away. The only thing you see before the last lights 
         go out is a silhouette of a person running away. A million questions rush into your head. 
         Who is this person? Why me? Why is the floor so floory? Right before you die you swore to find out who killed you. """, 0.001)
-        time.sleep(2)
+        time.sleep(5)
         clearConsole()
     else:
         print("Invalid name; please use characters only!")
@@ -37,23 +42,59 @@ def clearConsole():
     os.system(command)
 
 
-def chooseSys(Op1, Op2, Op3, Op4, Op5q, mainOptions = "Main"):
+def chooseSys(Op1, Op2, Op3, Op4, Menu = "Menu", Back = "Back"):
 
-    Options = [Op1, Op2, Op3, Op4, Op5q]
-    mainOptions = ["Inventory", "Map", "Quit"]
+    Options = [Op1, Op2, Op3, Op4, Menu, Back]
 
-    print(f"1 {Options[0]}: , 2 {Options[1]}: 3 {Options[2]}:, 4 {Options[3]}: 5 {Options[4]}")
+    print(
+        f"1 {Options[0]}: , 2 {Options[1]}: 3 {Options[2]}:, 4 {Options[3]}:, 5 {Options[4]} 6 {Options[5]}")
     
     try: 
      z = int(input("Choose : "))
-     if z == 1 or z == 2 or z == 3 or z == 4 or z == 5:
+     if z == 1 or z == 2 or z == 3 or z == 4 or z == 5 or z == 6:
         text = Options[(z-1)]
         return text
     except:
          print("Not an option loser")
+
+def menu(Op1 = "Inventory", Op2 = "Map", op3 = "Back"):
+
+    menuOptions = [Op1, Op2, op3]
+    while True:
+        print(f"{menuOptions[0]}, {menuOptions[1]}, {menuOptions[2]}")
+        z = input("Type the option you want: ").lower()
+        if z == "inventory":
+            while True:
+                print(f"{gameInventory}")
+                inventoryCheck = input("Type the item you'd like to inspect or enter q to quit inventory: ").lower()
+                if inventoryCheck == "q":
+                        break
+                if inventoryCheck in gameInventory:
+                    if inventoryCheck == "jar of milk":
+                        inputInventory = input("The jar of milk seems to be glowing bright, would you like to drink it? (Y/N) ").lower()
+                        if inputInventory == "y":
+                            gameInventory.remove("jar of milk")
+                            print("You feel the power of the milk flowing through your bloodstream. You have gained fire resistance")
+                            print("jar of milk has been removed from your inventory")
+                        elif inputInventory == "n":
+                            continue
+                        else:
+                            print("L bozo, this is not a valid input")
+                CQuit = input("Do you want to look into another option? (Y/N) ").lower()
+                if CQuit == "y":
+                    continue
+                else:
+                    break
+        elif z == "map":
+            print(f"You havent found a map yet")
+        elif z == "back":
+            break
+        else:
+            print("Not a valid option")
+
         
 
-def typeSys(xWords, time):
+def typeSys(xWords, time = 0.01):
     for char in xWords:
         sleep(time)
         sys.stdout.write(char)
@@ -101,6 +142,7 @@ def playSound(soundPath, volume = 0.01):
     print("music started playing....")
     #Play the music
     mixer.music.play()
+    
 
 
 
