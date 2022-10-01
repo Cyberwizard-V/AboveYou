343b8ce0d145ca502e1b91bb6ac436658a742790
Jeffrey Yeh <yehspam@outlook.com>
2022-10-01 15:56:27 +0200

    stage mountain

---
+++ stageMountain.py
@@ -0,0 +1,12 @@
+from re import X
+import functions
+import time
+import climage
+from functions import *
+from image import DrawImage
+
+#\033[0m ending quote for color
+
+
+def stageMountain():
+    stageMountain = True
